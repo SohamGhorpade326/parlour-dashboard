cf02401b16c56034e027963bb92dab75bb22c669
SohamGhorpade326 <sohamghorpade912@gmail.com>
2025-07-06 23:31:43 +0530

    Temporarily disabled ESLint strict rules for deployment

--- backend-parlour-api/src/routes/employee.routes.ts
+++ backend-parlour-api/src/routes/employee.routes.ts
@@ -8,5 +8,5 @@ router.get('/', authenticate, getAllEmployees);
 router.post('/', authenticate, authorize(['Super Admin']), addEmployee);
 router.delete('/:id', authenticate, authorize(['Super Admin']), deleteEmployee);
 router.put('/:id', authenticate, authorize(['Super Admin']), updateEmployee);
-router.delete('/:id', authenticate, authorize(['Super Admin']), deleteEmployee);
+
 export default router;
